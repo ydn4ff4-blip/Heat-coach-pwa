c447dc616c83b9b99bd7cef29f5549a0468688d2
ydn4ff4-blip <ydn4ff4@gmail.com>
2025-08-15 00:20:28 +0700

    Comit baru

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 const CACHE='heatcoach-pro-v1';
 const ASSETS=[
   './','./index.html','./style.css','./main.js',
-  './manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'
+  './manifest.webmanifest','./icons/icon-192x192.png','./icons/icon-512x512.png'
 ];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();
